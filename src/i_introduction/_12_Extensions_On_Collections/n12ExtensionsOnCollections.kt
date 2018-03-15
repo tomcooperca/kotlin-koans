ae35119d77e4876f5bb2baad3fe9692c69df288a
Thomas Cooper <thomas.cooper@rbc.com>
2018-03-14 23:56:42 -0400

    i-12

--- src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
+++ src/i_introduction/_12_Extensions_On_Collections/n12ExtensionsOnCollections.kt
@@ -17,7 +17,8 @@ fun todoTask12(): Nothing = TODO(
 )
 
 fun task12(): List<Int> {
-    todoTask12()
-    return arrayListOf(1, 5, 2)
+    val arrayList: ArrayList<Int> = arrayListOf(1, 5, 2)
+    arrayList.sortDescending()
+    return arrayList
 }
 
